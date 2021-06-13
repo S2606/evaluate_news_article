80c7fb5ea3e600696f6a7ff806333a34d4043113
S2606 <shagun.khemka60@gmail.com>
2021-06-13 19:10:36 +0530

    refactor: Add webpack related file

---
+++ webpack.dev.js
@@ -0,0 +1,45 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const WorkboxPlugin = require('workbox-webpack-plugin')
+
+module.exports = {
+    entry: './src/client/js/index.js',
+    output: {
+        libraryTarget: 'var',
+        library: 'AnalyzerClient'
+    }, 
+    mode: 'development',
+    devtool: 'source-map',
+    stats: 'verbose',  
+    module: {
+        rules: [
+            {
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+            }   
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+        }),
+        new CleanWebpackPlugin({
+            // Simulate the removal of files
+            dry: true,
+            // Write Logs to Console
+            verbose: true,
+            // Automatically remove all unused webpack assets on rebuild
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false
+        }),
+        new WorkboxPlugin.GenerateSW({
+            clientsClaim: true
+        })
+    ]
+}
---
+++ webpack.prod.js
@@ -0,0 +1,38 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin')
+
+module.exports = {
+    entry: './src/client/js/index.js',
+    output: {
+        libraryTarget: 'var',
+        library: 'AnalyzerClient'
+    }, 
+    mode: 'production',
+    optimization: {
+        minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    },
+    module: {
+        rules: [
+            {
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+        }),
+        new WorkboxPlugin.GenerateSW({
+            clientsClaim: true
+        })
+    ]
+}
